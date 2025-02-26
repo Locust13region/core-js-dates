cd8e795d7676c822e16e2a49a31eec169508b430
DESKTOP-MF8V5D1\Andrey <m>
2025-02-26 23:28:45 +0300

    feat: 9 passing

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -45,7 +45,7 @@ function getTime(...date) {
  * '2024-01-30T00:00:00.000Z' => 'Tuesday'
  */
 function getDayName(...date) {
-  return new Date(...date).toLocaleDateString('en-EN', { weekday: 'long' });
+  return new Date(...date).toLocaleDateString('en-US', { weekday: 'long' });
 }
 
 /**
@@ -76,10 +76,9 @@ function getNextFriday(date) {
  * 1, 2024 => 31
  * 2, 2024 => 29
  */
-function getCountDaysInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountDaysInMonth(month, year) {
+  return new Date(year, month, 0).getDate();
 }
-
 /**
  * Returns the total number of days between two dates, including both the start and end dates.
  *
@@ -91,10 +90,9 @@ function getCountDaysInMonth(/* month, year */) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  return (new Date(dateEnd) - new Date(dateStart)) / (1000 * 60 * 60 * 24) + 1;
 }
-
 /**
  * Returns true if a given date is within a specified range, including both the start and end dates.
  *
@@ -112,10 +110,12 @@ function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
  * '2024-02-02', { start: '2024-02-02', end: '2024-03-02' } => true
  * '2024-02-10', { start: '2024-02-02', end: '2024-03-02' } => true
  */
-function isDateInPeriod(/* date, period */) {
-  throw new Error('Not implemented');
+function isDateInPeriod(date, period) {
+  const checkDate = new Date(date);
+  const startDate = new Date(period.start);
+  const endDate = new Date(period.end);
+  return checkDate >= startDate && checkDate <= endDate;
 }
-
 /**
  * Returns the date formatted in 'M/D/YYYY, hh:mm:ss a'.
  *
@@ -127,10 +127,19 @@ function isDateInPeriod(/* date, period */) {
  * '1999-01-05T02:20:00.000Z' => '1/5/1999, 2:20:00 AM'
  * '2010-12-15T22:59:00.000Z' => '12/15/2010, 10:59:00 PM'
  */
-function formatDate(/* date */) {
-  throw new Error('Not implemented');
+function formatDate(date) {
+  const currentDate = new Date(date);
+  return currentDate.toLocaleDateString('en-US', {
+    month: 'numeric',
+    day: 'numeric',
+    year: 'numeric',
+    hour: 'numeric',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: true,
+    timeZone: 'UTC',
+  });
 }
-
 /**
  * Returns the total number of weekend days (Saturdays and Sundays) in a specified month and year.
  *
@@ -143,10 +152,16 @@ function formatDate(/* date */) {
  * 12, 2023 => 10
  * 1, 2024 => 8
  */
-function getCountWeekendsInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountWeekendsInMonth(month, year) {
+  let totalWeekendDays = 0;
+  const daysInMonth = new Date(year, month, 0).getDate();
+  for (let i = 1; i <= daysInMonth; i += 1) {
+    const current = new Date(year, month - 1, i);
+    const dayOfWeek = current.getDay();
+    if (dayOfWeek === 0 || dayOfWeek === 6) totalWeekendDays += 1;
+  }
+  return totalWeekendDays;
 }
-
 /**
  * Returns the week number of the year for a given date.
  * The first week of the year is defined according to ISO8601.
